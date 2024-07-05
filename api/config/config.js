d8afffc20d47a6e62eae1701b035d6e2580dd5cf
Hamza Ashfaq <hamzaashfaq7866@gmail.com>
2024-07-05 11:23:57 -0700

    changes done

---
+++ api/config/config.js
@@ -0,0 +1,34 @@
+import SES from "aws-sdk/clients/ses.js";
+import S3 from "aws-sdk/clients/s3.js";
+import dotenv from 'dotenv'
+dotenv.config()
+
+export const DATABASE_LOCAL = "mongodb://127.0.0.1/apnaghar"
+
+
+export const DATABASE_CLOUD = process.env.MONGODB_ATLAS
+
+export const EMAIL_FROM = process.env.EMAIL_FROM
+export const REPLY_TO = process.env.REPLY_TO
+
+export const AWS_REGION=  process.env.AWS_REGION
+export const AWS_API_VERSION=  process.env.AWS_API_VERSION
+
+const awsConfig = {
+    accessKeyId: AWS_ACCESS_KEY_ID,
+    secretAccessKey: AWS_SECRET_ACCESS_KEY,
+    region: AWS_REGION,
+    apiVersion: AWS_API_VERSION
+}
+
+export const AWSSES = new SES(awsConfig);
+export const AWSS3 = new S3(awsConfig);
+
+
+export const CLIENT_URL = process.env.CLIENT_URL
+export const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY
+
+//// twilio 
+
+// export the Twilio client so that it can be used elsewhere in our code
+// export const client = require('twilio')(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
--- api/middlewares/authMiddleware.js
+++ api/middlewares/authMiddleware.js
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
-import {JWT_SECRET}  from "../config/config.js";
+import {JWT_SECRET_KEY}  from "../config/config.js";
 
 export const requiredLoggedIn = (req, res, next) => {
     try {
